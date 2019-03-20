084c2cbc700f8d616ad9f8fa6263dfd27de99c63
nakia12321 <43543837+nakia12321@users.noreply.github.com>
2019-03-20 10:09:10 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+ $("button").click(function() {
+    var seafood=$(".seafood").val();
+    var candy=$(".candy").val();
+    var  cream=$(".ice cream").val();
+    $(".seafoodline").text("You will eat seafood 4 times a week");
+    $(".candyline").text("You will eat  2 times a day");
+    $(".icecreamline").text("You will eat ice cream 3 times a week");
+});
